211fd1108da4ea01a90ce74e2a4f7e6e1ae1e06d
Pathum Liyanagama <pathumpml.dev@gmail.com>
2019-04-19 09:24:11 +0530

    Add some gulp watch tasks for style changes

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+simplevars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+gulp.task('default', function() {
+    console.log("Default task");
+});
+
+gulp.task('html', function(){
+    console.log("html task running");
+});
+
+gulp.task('style', function(){
+    gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([nested, simplevars, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles/'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+        gulp.start('html');
+    });
+
+    watch('./app/assets/styles/**/*.css', function(){
+        gulp.start('style');
+    });
+});
